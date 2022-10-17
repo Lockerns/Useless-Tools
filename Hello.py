ff16713c8569f04c9ff205383441349b4d017623
wzc <wzcong77@163.com>
2022-10-17 09:09:38 +0800

    matplotlib

--- Hello.py
+++ Hello.py
@@ -1,16 +1,17 @@
-import re
-import pyperclip
+import matplotlib.pyplot as plt
+import numpy as np
 
-string = "  * it     is blank    space     test *  "
-str1 = 'dfadf   d\n \n\n \nfa  ds '
+x = np.arange(9, 11, 0.001)
 
+y1 = pow(2.718281828, -2.291969677 * x) * pow(10, 9)
 
-str_new = re.sub('[\n]+', '\n', str1)
-# str_new4 = re.sub(r"\s+", " ", string)  # 多个连续空格合并成一个空格
+fig, ax1 = plt.subplots()
 
-text = pyperclip.paste()
+ax1.set_xlabel('gap distance w [Å]', color='black')
+ax1.set_ylabel('IT [a.u.]', color='black')
+ax1.plot(x, y1, 'blue')
+ax1.set_ylim(0, 1.2)  # 设置y1范围
 
-space_d = re.compile(r'\s+')
-str_new4 = space_d.sub(' ', text)
-pyperclip.copy(str_new4)
-# print(str_new4)
+plt.xlim((9, 11))  # 设置x范围
+plt.show()
+# plt.savefig('1.png')
